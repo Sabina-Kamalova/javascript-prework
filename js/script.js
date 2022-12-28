dec12b6ce241ab1b95413f540f5add581d62c589
Sabina Kamalova <skamalova09@gmail.com>
2022-12-28 23:59:02 +0100

    Add script-PlayerMove

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = randomNumber;
+let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
@@ -15,3 +15,22 @@ else if(randomNumber == 3){
   }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput =='2'){
+    playerMove = 'papier';
+}
+else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
