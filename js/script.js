f3a6d0c4ff6e8fe758a02ee54b66d42e95e4b4e0
Sabina Kamalova <skamalova09@gmail.com>
2022-12-31 21:31:54 +0100

    Add result

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+function playGame(playerInput){
+  clearMessages();
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 function getMoveName(argMoveId){
   if(argMoveId == 1){
     return 'kamień';
@@ -10,21 +11,16 @@ function getMoveName(argMoveId){
   else if(argMoveId == 3){
     return 'nożyce';
   }
-
   printMessage('Nie znam ruchu ' + argMoveId + '.');
   return 'nieznany ruch';
 }
-
 let computerMove = getMoveName(randomNumber);
-
 printMessage('Mój ruch to: ' + computerMove);
-
+/*
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
+*/
 let playerMove = getMoveName(playerInput);
-
 printMessage('Twój ruch to: ' + playerMove);
-
 function displayResult(){
   printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
   if(computerMove == 'kamień' && playerMove == 'papier'){
@@ -45,3 +41,14 @@ function displayResult(){
   }
 }
   displayResult();
+}
+
+  document.getElementById('play-rock').addEventListener('click', function(){
+    playGame('1');
+  });
+  document.getElementById('play-paper').addEventListener('click', function(){
+    playGame('2');
+  });
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame('3');
+  });
